477f5da45c760fe2524a78c691803aa41850cf66
Gabriel Brunetto <gabriel.quadros.pb@compasso.com.br>
2024-07-18 19:42:11 -0300

    Add slider on the project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 
 import './App.css';
 import Settings from './Settings';
+import SettingsContext from './SettingsContext';
 import Timer from './timer';
 import { useState } from 'react';
 
 function App() {
 
   const[showSettings, setShowSettings] = useState(true)
+  const {workMinutes, setWorkMinutes} = useState (45);
+  const {breakMinutes, setbreakMinutes} = useState (15);
 
   return (
     <main>
-      {showSettings ? <Settings/> : <Timer/>}
+      <SettingsContext.Provider value={{
+        workMinutes, 
+        breakMinutes,
+        setWorkMinutes,
+        setbreakMinutes,
+      }}>
+        {showSettings ? <Settings/> : <Timer/>}
+      </SettingsContext.Provider>
+      
     </main>
   );
 }
--- src/Settings.js
+++ src/Settings.js
@@ -1,8 +1,33 @@
+import ReactSlider from "react-slider";
+import './slider.css';
+import { useContext } from 'react';
+import SettingsContext from "./SettingsContext";
+
+
 function Settings(){
+    const SettingsInfo = useContext(SettingsContext);
     return(
         <div style={{textAlign: 'left'}}>
-            <label>Work Minutes:</label>
-            <label>Break Minutes</label>
+            <label>Work: {SettingsInfo.workMinutes} </label>
+            <ReactSlider
+                className={'slider'}
+                thumbClassName={'thumb'}
+                trackClassName={'track'}    
+                value={SettingsInfo.workMinutes}  
+                onChange={newValue => SettingsInfo.setWorkMinutes(newValue)}  
+                min={1}
+                max={120}
+            />
+            <label>Break: {SettingsInfo.breakMinutes}</label>
+            <ReactSlider
+                className={'slider green'}
+                thumbClassName={'thumb'}
+                trackClassName={'track'}    
+                value={SettingsInfo.breakMinutes}
+                onChange={newValue => SettingsInfo.setBreakMinutes(newValue)}     
+                min={1}
+                max={120}
+            />
         </div>
     );
 }
